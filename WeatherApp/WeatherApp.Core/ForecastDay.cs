66def0d505003e6fe8c2c8f739e0ac34b7e3681d
CV <0caitlinvan0@gmail.com>
2026-08-09 19:32:36 +0200

    Properties

--- WeatherApp/WeatherApp.Core/ForecastDay.cs
+++ WeatherApp/WeatherApp.Core/ForecastDay.cs
@@ -2,5 +2,8 @@
 
 public class ForecastDay
 {
-    
+    public DateTime date { get; set; }
+    public double maxTemperature { get; set; }
+    public double minTemperature { get; set; }
+    public int weatherCode { get; set; }
 }

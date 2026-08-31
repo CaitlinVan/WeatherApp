2a057e1741504b5feb88fe8eca8c923fcec14a15
CaitlinVan <0caitlinvan0@gmail.com>
2026-08-31 15:47:22 +0200

    new edits to IWeatherServices.cs'

--- WeatherApp/WeatherApp.Infrastructure/WeatherServices.cs
+++ WeatherApp/WeatherApp.Infrastructure/WeatherServices.cs
@@ -8,6 +8,17 @@ public class WeatherServices(HttpClient httpClient) : IWeatherService
 {
     public async Task<WeatherForecast> GetWeatherForecastAsync(string cityName, CancellationToken ct = default)
     {
+        var location = await FindCityAsync(cityName, ct);
         
+        var url = $"https://api.open-meteo.com/v1/forecast" +
+                  $"?latitude={location.Latitude}&longitude={location.Longitude}" +
+                  $"&current=temperature_2m,wind_speed_10m,weather_code" +
+                  $"&daily=temperature_2m_max,temperature_2m_min,weather_code" +
+                  $"&timezone=auto";
+
+
+
+        return new WeatherForecast(location); 
+
     }
 }
